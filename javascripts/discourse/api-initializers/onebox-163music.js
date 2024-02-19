a77238a0d3289fe98f21fcc3f0e365b71cf639fa
PangBo <51732678+pangbo13@users.noreply.github.com>
2024-02-19 19:35:26 +0800

    feat: add support for netease music

---
+++ javascripts/discourse/api-initializers/onebox-163music.js
@@ -0,0 +1,69 @@
+import { apiInitializer } from "discourse/lib/api";
+import I18n from "I18n";
+
+export default apiInitializer("0.11.1", api => {
+
+    const getIframeUrl = (src) => {
+        const url = new URL(src, document.baseURI);
+        const iframeUrl = new URL('//music.163.com/outchain/player', document.baseURI);
+        
+        url.searchParams.forEach((v, k) => {
+            if(['id', 'type', 'height'].includes(k)){
+                iframeUrl.searchParams.set(k, v);
+            }
+        });
+        iframeUrl.searchParams.set('auto', '0');
+        
+        return iframeUrl.toString();
+    };
+
+    const createOrResetIframe = (iframe) => { 
+        if (iframe === undefined) { 
+            return document.createElement('iframe');
+        } else {
+            // remove all attributes
+            while (iframe.attributes.length > 0) {
+                iframe.removeAttribute(iframe.attributes[0].name);
+            }
+            return iframe;
+        }
+    }
+
+    const decorateIframe = (iframe_src, original_iframe) => {
+        const iframe = createOrResetIframe(original_iframe);
+        iframe.classList.add('onebox-163music-iframe');
+        iframe.setAttribute('src',iframe_src);
+        iframe.setAttribute('sandbox', 'allow-scripts allow-same-origin');
+        iframe.setAttribute('referrerpolicy', 'no-referrer');
+        iframe.setAttribute('loading', 'lazy');
+        iframe.setAttribute('importance', 'low');
+        return iframe;
+    };
+
+    const createOneboxIframe = (src, original_iframe, type, height) => {
+        decorateIframe(getIframeUrl(src), original_iframe);
+        original_iframe.classList.add('onebox', 'onebox-163music');
+        if (type === 1) {
+            original_iframe.classList.add('onebox-163music-song');
+        } else if (type === 2) {
+            original_iframe.classList.add('onebox-163music-playlist');
+        }
+        original_iframe.setAttribute('height', height);
+        original_iframe.setAttribute('data-onebox-src', src);
+        return original_iframe;
+    };
+
+    api.decorateCookedElement(elem => {
+        elem.querySelectorAll("iframe").forEach((iframe_elem) => {
+            const src = iframe_elem.getAttribute("src");
+            const url = new URL(src, document.baseURI);
+            const rendered_oneboxes = [];
+            if(url.hostname === 'music.163.com' && url.pathname === '/outchain/player'){
+                createOneboxIframe(src, iframe_elem, url.searchParams.get('type'), iframe_elem.getAttribute("height"));
+                rendered_oneboxes.push(iframe_elem);
+            }
+        });
+    },
+    { id: '163music-onebox-decorator', onlyStream: true }
+  );
+});
--- javascripts/discourse/api-initializers/onebox-bilibili.js
+++ javascripts/discourse/api-initializers/onebox-bilibili.js
@@ -22,9 +22,21 @@ export default apiInitializer("0.11.1", api => {
         return iframeUrl.toString();
     };
 
-    const createIframe = (iframe_src) => {
-        const biliframe = document.createElement('iframe');
-        biliframe.classList.add('bilibili-onebox-iframe')
+    const createOrResetIframe = (iframe) => { 
+        if (iframe === undefined) { 
+            return document.createElement('iframe');
+        } else {
+            // remove all attributes
+            while (iframe.attributes.length > 0) {
+                iframe.removeAttribute(iframe.attributes[0].name);
+            }
+            return iframe;
+        }
+    }
+
+    const decorateIframe = (iframe_src, original_iframe) => {
+        const biliframe = createOrResetIframe(original_iframe)
+        biliframe.classList.add('onebox-bilibili-iframe')
         biliframe.setAttribute('src',iframe_src);
         biliframe.setAttribute('sandbox', 'allow-scripts allow-same-origin');
         biliframe.setAttribute('referrerpolicy', 'no-referrer');
@@ -33,14 +45,22 @@ export default apiInitializer("0.11.1", api => {
         return biliframe;
     };
 
-    const createOnebox = (src) => {
+    const createOneboxDiv = (src) => {
         const onebox_div = document.createElement('div');
         onebox_div.classList.add('onebox', 'onebox-bilibili');
         onebox_div.setAttribute('data-onebox-src', src);
-        const biliframe = createIframe(getIframeUrl(src));
+        const biliframe = decorateIframe(getIframeUrl(src));
         onebox_div.replaceChildren(biliframe);
         return onebox_div;
     };
+
+    const createOneboxIframe = (src, original_iframe) => { 
+        decorateIframe(getIframeUrl(src), original_iframe);
+        original_iframe.setAttribute('data-onebox-src', src);
+        original_iframe.classList.add('onebox', 'onebox-bilibili');
+        return original_iframe;
+    }
+
 
     api.decorateCookedElement(elem => {
         const regex = /^https?:\/\/(?:www\.)?bilibili\.com\/video\/([a-zA-Z0-9]+)(?:\/.*|\?.*)?$/;
@@ -50,7 +70,7 @@ export default apiInitializer("0.11.1", api => {
         oneboxes.forEach((onebox_elem) => {
             const src = onebox_elem.getAttribute("data-onebox-src") ?? onebox_elem.getAttribute("href");
             if(regex.test(src)){
-                const onebox_div = createOnebox(src);
+                const onebox_div = createOneboxDiv(src);
                 onebox_elem.replaceWith(onebox_div);
                 rendered_oneboxes.push(onebox_div);
             }
@@ -58,9 +78,8 @@ export default apiInitializer("0.11.1", api => {
         iframes.forEach((iframe_elem) => {
             const src = iframe_elem.getAttribute("src");
             if((new URL(src,document.baseURI)).hostname === 'player.bilibili.com'){
-                const onebox_div = createOnebox(src);
-                iframe_elem.replaceWith(onebox_div);
-                rendered_oneboxes.push(onebox_div);
+                createOneboxIframe(src, iframe_elem);
+                rendered_oneboxes.push(iframe_elem);
             }
         });
     },
